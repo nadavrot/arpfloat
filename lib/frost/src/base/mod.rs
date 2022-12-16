e80db81d0ad32c890069463752443c9027d35366
Nadav Rotem <nadav256@gmail.com>
2022-12-16 12:21:34 -0800

    Add a simple float struct.

--- lib/frost/src/base/mod.rs
+++ lib/frost/src/base/mod.rs
@@ -1,15 +1,23 @@
-
 #[derive(Debug, Clone, Copy)]
-pub enum Sign {
-    Positive,
-    Negative,
+
+pub struct Float<const EXPONENT: usize, const SIGNIFICANT: usize> {
+    // The sign bit.
+    sign: bool,
+    // The expotent.
+    exp: u64,
+    // The significant.
+    sig: u64,
 }
 
-impl Sign {
-    pub fn is_positive(&self) -> bool {
-        match self {
-            Sign::Positive => true,
-            Sign::Negative => false,
-        }
+impl<const EXPONENT: usize, const SIGNIFICANT: usize>
+    Float<EXPONENT, SIGNIFICANT>
+{
+    pub fn new(sign: bool, exp: u64, sig: u64) -> Float<EXPONENT, SIGNIFICANT> {
+        Float { sign, exp, sig }
     }
+}
+
+#[test]
+fn constructor_test() {
+    let _: Float<6, 10> = Float::new(false, 10, 123);
 }
