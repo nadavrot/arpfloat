161cd1854fdb35bc286900b4c0602678b14ee635
Nadav Rotem <nadav256@gmail.com>
2022-12-16 16:23:49 -0800

    Add fp32 parsing.

--- lib/frost/src/base/mod.rs
+++ lib/frost/src/base/mod.rs
@@ -1,11 +1,10 @@
 #[derive(Debug, Clone, Copy)]
-
 pub struct Float<const EXPONENT: usize, const SIGNIFICANT: usize> {
     // The Sign bit.
     sign: bool,
     // The Exponent.
     exp: u64,
-    // The Significant.
+    // The Integral Significant.
     sig: u64,
 }
 
@@ -25,6 +24,17 @@ impl<const EXPONENT: usize, const SIGNIFICANT: usize>
         a.set_sign(sign);
         a.set_exp(exp);
         a.set_significant(sig);
+        a
+    }
+
+    pub fn from_u32_float(float: u32) -> Float<EXPONENT, SIGNIFICANT> {
+        let integral = float & 0x7fffff;
+        let exp: i64 = ((float >> 23) & 0xff) as i64;
+        let sign = float >> 31;
+        let mut a = Self::default();
+        a.set_sign(sign == 1);
+        a.set_exp(exp - 127);
+        a.set_significant(integral as u64);
         a
     }
 
@@ -52,14 +62,14 @@ impl<const EXPONENT: usize, const SIGNIFICANT: usize>
 
     /// \returns the biased exponent.
     pub fn get_exp(&self) -> i64 {
-        self.exp as i64 + Self::get_bias() as i64
+        self.exp as i64 - Self::get_bias() as i64
     }
 
     /// Sets the biased exponent to \p new_exp.
     pub fn set_exp(&mut self, new_exp: i64) {
         let exp_min: i64 = -(Self::get_bias() as i64);
         let exp_max: i64 = ((1 << EXPONENT) - Self::get_bias()) as i64;
-        let new_exp: i64 = new_exp - (Self::get_bias() as i64);
+        let new_exp: i64 = new_exp + (Self::get_bias() as i64);
         assert!(new_exp <= exp_max);
         assert!(exp_min <= new_exp);
         self.exp = new_exp as u64
@@ -70,25 +80,51 @@ impl<const EXPONENT: usize, const SIGNIFICANT: usize>
     }
 
     pub fn as_f64(&self) -> f64 {
-        0.0
+        let exp = self.get_exp();
+        let significant = (1 << SIGNIFICANT) + self.get_significant();
+        let shift = 1 << SIGNIFICANT;
+        let significant: f64 = (significant as f64) / (shift as f64);
+        let e2 = f64::powf(2., exp as f64);
+
+        let mut val: f64 = significant * e2;
+        if self.get_sign() {
+            val = -val;
+        }
+        val
+    }
+
+    pub fn dump(&self) {
+        let exp = self.get_exp();
+        let significant = self.get_significant();
+        let sign = self.get_sign() as usize;
+        println!("FP[{} : {} : {}]", sign, exp, significant);
     }
 }
 
-type FP16 = Float<5, 11>;
-type FP32 = Float<8, 24>;
-type FP64 = Float<11, 43>;
+pub type FP16 = Float<5, 11>;
+pub type FP32 = Float<8, 24>;
+pub type FP64 = Float<11, 43>;
 
 #[test]
-fn constructor_test() {
+fn setter_test() {
     assert_eq!(FP16::get_bias(), 15);
     assert_eq!(FP32::get_bias(), 127);
     assert_eq!(FP64::get_bias(), 1023);
 
-    let a: Float<6, 10> = Float::new(false, 10, 123);
-    let _ = a.as_f64();
-    assert_eq!(a.get_sign(), false);
-    assert_eq!(a.get_significant(), 123);
-    assert_eq!(a.get_exp(), 10);
+    let a: Float<6, 10> = Float::new(false, 2, 12);
+    let mut b = a;
+    b.set_exp(b.get_exp());
+    assert_eq!(a.get_exp(), b.get_exp());
+}
 
-    let _a = FP16::new(false, 12, 23);
+#[test]
+fn constructor_test() {
+    let inputs: [u32; 5] = [0x3f800000, 0x40800000, 0x3f000000, 4, 5];
+    let outputs: [f64; 5] = [1.0, 4.0, 0.5, 4., 5.];
+
+    for i in 0..3 {
+        let a = FP32::from_u32_float(inputs[i]);
+        a.dump();
+        assert_eq!(a.as_f64(), outputs[i]);
+    }
 }
