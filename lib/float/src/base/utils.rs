b36ded6ce3efb3723fdc3a8a38faef0b40e36153
Nadav Rotem <nadav256@gmail.com>
2022-12-23 16:38:04 -0800

    Remove an unused method.

--- lib/float/src/base/utils.rs
+++ lib/float/src/base/utils.rs
@@ -10,32 +10,6 @@ fn test_masking() {
     assert_eq!(mask(8), 255);
 }
 
-/// Convert a mantissa in the implicit format (no possible leading 1 bit) to
-/// the internal storage format. If \p leading_1 is set then a leading one is
-/// added (otherwise it is a subnormal).
-/// Format: [1 IIIIII 00000000]
-pub fn expand_mantissa_to_explicit<const FROM: usize>(
-    input: u64,
-    leading_1: bool,
-) -> u64 {
-    let value: u64 = if leading_1 { 1 << 63 } else { 0 };
-    let shift = 63 - FROM;
-    value | (input << shift)
-}
-
-#[test]
-fn test_expand_mantissa() {
-    assert_eq!(expand_mantissa_to_explicit::<8>(0, true), 1 << 63);
-    assert_eq!(
-        expand_mantissa_to_explicit::<8>(1, true),
-        0x8080000000000000
-    );
-    assert_eq!(
-        expand_mantissa_to_explicit::<32>(0xffffffff, false),
-        0x7fffffff80000000
-    );
-}
-
 // \returns the bias for this Float type.
 pub fn compute_ieee745_bias(exponent_bits: usize) -> usize {
     (1 << (exponent_bits - 1)) - 1
