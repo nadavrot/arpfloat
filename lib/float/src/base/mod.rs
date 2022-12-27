fb778350fe001c7764239fef0f9f76c0ad106944
Nadav Rotem <nadav256@gmail.com>
2022-12-26 21:47:40 -0800

    cleanups

--- lib/float/src/base/mod.rs
+++ lib/float/src/base/mod.rs
@@ -6,4 +6,4 @@ mod utils;
 
 pub use self::bigint::BigInt;
 pub use self::float::{Float, FP128, FP16, FP32, FP64};
-pub use self::utils::{get_special_test_values, Lfsr};
+pub use self::utils::Lfsr;
--- lib/float/src/base/utils.rs
+++ lib/float/src/base/utils.rs
@@ -10,6 +10,7 @@ fn test_masking() {
     assert_eq!(mask(8), 255);
 }
 
+#[allow(dead_code)]
 /// \returns list of interesting values that various tests use to catch edge cases.
 pub fn get_special_test_values() -> [f64; 20] {
     [
