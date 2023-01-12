e7bc2c7b906d314cb55a642b20f3f2d807296cb7
Nadav Rotem <nadav256@gmail.com>
2023-01-11 21:33:38 -0800

    Add a small program that prints e with 5k bits of accuracy.

---
+++ examples/print_bench.rs
@@ -0,0 +1,12 @@
+use arpfloat::{Float, Semantics};
+
+///! Calculates long numbers and prints them.
+///!  cargo run --example print_bench --release
+
+
+fn main() {
+    let sem = Semantics::new(32, 5000);
+    let val = Float::e(sem);
+    println!("F64: {}", val.as_f64());
+    println!("FP*: {}", val);
+}
