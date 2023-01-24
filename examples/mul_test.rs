3e4f853c86a95c3dcb724e04acb72b6424dab0ed
Nadav Rotem <nadav256@gmail.com>
2023-01-24 11:47:44 -0800

    Add a big-int mul benchmark.

---
+++ examples/mul_test.rs
@@ -0,0 +1,33 @@
+use std::env;
+
+use arpfloat::BigInt;
+
+///! Calculates long numbers and prints them.
+///!  cargo run --example mul_test --release 100
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let digits: usize;
+
+    match args.len() {
+        2 => match args[1].parse::<usize>() {
+            Ok(x) => digits = x,
+            Err(_) => {
+                println!("Not an integer");
+                return;
+            }
+        },
+        _ => {
+            println!("Usage: test_mul [num_digits]");
+            return;
+        }
+    }
+
+    let mut a = BigInt::pseudorandom(digits);
+    let b = BigInt::pseudorandom(digits);
+
+    println!("Multiplying two {}-bit numbers", digits * 64);
+    a.inplace_mul(&b);
+    println!("Done");
+}
